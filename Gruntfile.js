3596b654297fec71f7427677ec433bbabba007e9
Greg Lucas <magellanarts@gmail.com>
2018-05-16 21:01:19 -0700

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    //compass
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'dev/sass',
+          cssDir: 'target',
+          specify: 'dev/sass/style.scss',
+          imagesDir: 'dev/i/'
+        }
+      }
+    },
+
+    copy: {
+      dist: {
+        files: [
+          {
+            expand: true,
+            cwd: 'dev',
+            src: ['*.html', '*.php', 'js/**/*', 'page-templates/*.php', 'components/*.php', 'i/**/*', 'fonts/**/*', 'data/**/*'],
+            dest: 'target/'
+          }
+        ]
+      }
+    },
+
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 versions', 'ie 8', 'ie 9', 'ie 10']
+      },
+
+      dist: {
+        src: 'target/style.css'
+      }
+
+    },
+
+    clean: {
+      dist: ['target/']
+
+    },
+
+    watch: {
+      files: ['dev/**'],
+      tasks: ['default'],
+      options: {
+        livereload: true
+      }
+    }
+
+  });
+
+  //Load in plugins
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  //grunt.loadNpmTasks('grunt-browserify');
+
+  grunt.registerTask('default', ['clean', 'copy', 'compass', 'autoprefixer']);
+}
---
+++ dev/header.php
@@ -0,0 +1,7 @@
+<!doctype html>
+<html>
+<head>
+    <title>ROOT</title>
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
+</head>
+<body>
