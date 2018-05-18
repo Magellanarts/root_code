b39ccd194292440c88268725ac38e0fd8d48a1ef
Greg Lucas <magellanarts@gmail.com>
2018-05-18 10:01:24 -0700

    studio landing page

--- dev/studio-nyc.php
+++ dev/studio-nyc.php
@@ -1,19 +1,36 @@
 <?php include('header.php'); ?>
 <section class="l-content-container c-hero js-hero u-img-fill-area">
-  <h1>A fully-integrated production photo house - a modern partner to pioneering brands.</h1>
-
-
+  <h1>NYC Studios</h1>
 </section>
 
+<section class="c-thumbnails">
+  <a href="#" class="c-thumbnail u-img-fill-area" style="background-image: url(i/root-digital-suite@2x.jpg)">
+    <span class="c-thumbnail__title">Studio 1</span>
+  </a>
+  <a href="#" class="c-thumbnail u-img-fill-area" style="background-image: url(i/equipment-rentals@2x.jpg)">
+    <span class="c-thumbnail__title">Studio 2</span>
+  </a>
+  <a href="#" class="c-thumbnail u-img-fill-area" style="background-image: url(i/root-digital-suite@2x.jpg)">
+    <span class="c-thumbnail__title">Studio 3</span>
+  </a>
+  <a href="#" class="c-thumbnail u-img-fill-area" style="background-image: url(i/creative-bts@2x.jpg)">
+    <span class="c-thumbnail__title">Studio 1</span>
+  </a>
+  <a href="#" class="c-thumbnail u-img-fill-area" style="background-image: url(i/root-digital-suite@2x.jpg)">
+    <span class="c-thumbnail__title">Cafe Lobby</span>
+  </a>
+  <a href="#" class="c-thumbnail u-img-fill-area" style="background-image: url(i/equipment-rentals@2x.jpg)">
+    <span class="c-thumbnail__title">Roof Deck</span>
+  </a>
+</section>
 
-<section class="l-page-section l-content-container">
+<section class="l-page-section l-content-container u-lrg-text o-content">
       <div class="c-tag">Root NYC</div>
       <h2>Based in the premier art district of Chelsea, ROOT NYC is where it all started.</h2>
 
   <p>The first of our two locations, our Manhattan studios has been serving industry clients in fashion, arts, and entertainment for nearly two decades. Offering three studios and a roof deck, spanning over 10,000 sq. feet.</p>
 
   <p>Close to  the A, C, E lines and a few blocks away from The Highline, our NYC studios are a prime location for innovative sets and VIP/celebrity events.</p>
-
 </section>
 
 
