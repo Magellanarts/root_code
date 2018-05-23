e4e55559956d0c419c01b922c1e06524d9ec10bd
Greg Lucas <magellanarts@gmail.com>
2018-05-22 17:33:31 -0700

    contact page and mc form

--- dev/contact.php
+++ dev/contact.php
@@ -13,53 +13,87 @@
 
 <section class="l-anchor-right l-split-content l-split-content--equal-width">
   <div>
-    <form>
-      <div class="l-split-content">
-        <div class="c-form-field c-form-field--half">
-          <select class="js-select" name="location">
-            <option>Please select a location*</option>
-            <option value="NYC">NYC</option>
-            <option value="BKN">BKN</option>
-          </select>
+    <div class="c-contact-form-container">
+      <form name="contact-form" class="c-contact-form js-form">
+        <div class="l-split-content">
+          <div class="c-form-field c-form-field--half">
+            <select class="js-select" name="location">
+              <option>Please select a location*</option>
+              <option value="NYC">NYC</option>
+              <option value="BKN">BKN</option>
+            </select>
+          </div>
+
+          <div class="c-form-field c-form-field--half">
+            <select class="js-select" name="department">
+              <option>Please select a department*</option>
+              <option value="NYC">Art</option>
+              <option value="BKN">Photography</option>
+            </select>
+          </div>
         </div>
 
-        <div class="c-form-field c-form-field--half">
-          <select class="js-select" name="department">
-            <option>Please select a department*</option>
-            <option value="NYC">NYC</option>
-            <option value="BKN">BKN</option>
-          </select>
+        <div class="c-form-field">
+          <input type="text" placeholder="Name*" name="name" required>
         </div>
+
+        <div class="c-form-field">
+          <input type="text" placeholder="Company name" name="company-name">
+        </div>
+
+        <div class="c-form-field">
+          <input type="email" placeholder="E-mail address*" name="email" required>
+        </div>
+
+        <div class="c-form-field">
+          <textarea placeholder="Message*" required></textarea>
+        </div>
+
+        <button class="c-btn" type="submit">Submit</button>
+
+      </form>
+
+      <div class="c-contact-form__success js-form-success u-lrg-text o-content">
+        <p>Thanks for reaching out.</p>
+
+        <p>We'll be in touch soon.</p>
       </div>
+    </div>
 
-      <div class="c-form-field">
-        <input type="text" placeholder="Name*" name="name" required>
-      </div>
-
-      <div class="c-form-field">
-        <input type="text" placeholder="Company name" name="company-name">
-      </div>
-
-      <div class="c-form-field">
-        <input type="email" placeholder="E-mail address*" name="email" required>
-      </div>
-
-      <div class="c-form-field">
-        <textarea placeholder="Message*" required></textarea>
-      </div>
-
-      <button class="c-btn" type="submit">Submit</button>
-
-    </form>
 
     <div class="l-split-content l-split-content--equal-width">
       <div>
-        <p>Isaac Litchfield, Owner
-          Isaac@rootnyc.com</p>
+        <div class="c-member">
+          <div class="c-member__name">Isaac Litchfield, Owner</div>
+          <a class="c-member__email" href="mailto:Isaac@rootnyc.com">Isaac@rootnyc.com</a>
+        </div>
+
+        <div class="c-member">
+          <div class="c-member__name">Ken Kobayashi, Executive Director</div>
+          <a class="c-member__email" href="mailto:Ken@rootnyc.com">Ken@rootnyc.com</a>
+        </div>
+
+        <div class="c-member">
+          <div class="c-member__name">Alex Strohmeier, Digital Director</div>
+          <a class="c-member__email" href="mailto:Alex@rootnyc.com">Alex@rootnyc.com</a>
+        </div>
+
       </div>
       <div>
-        <p>Isaac Litchfield, Owner
-          Isaac@rootnyc.com</p>
+        <div class="c-member">
+          <div class="c-member__name">Steven Fischbein, Executive Director</div>
+          <a class="c-member__email" href="mailto:Steven@rootnyc.com">Steven@rootnyc.com</a>
+        </div>
+
+        <div class="c-member">
+          <div class="c-member__name">Eric Coles, Bookings Director</div>
+          <a class="c-member__email" href="mailto:Eric@rootnyc.com">Eric@rootnyc.com</a>
+        </div>
+
+        <div class="c-member">
+          <div class="c-member__name">Lynn Guarino, Director of Motion Production</div>
+          <a class="c-member__email" href="mailto:Lynn@rootnyc.com">Lynn@rootnyc.com</a>
+        </div>
       </div>
     </div>
   </div>
@@ -70,11 +104,19 @@
   </div>
 </section>
 
-<section class="l-page-section">
+<section class="l-page-section l-content-container">
+  <div class="l-split-content">
+    <h3>@ROOTSTUDIOS</h3>
 
+    <div>
+      <a href="https://instagram.com/rootstudios/" target="_blank" class="u-hover-link u-hover-link--right">See All</a>
+    </div>
+  </div>
 
+  <div id="instafeed"></div>
 </section>
 
 
-
+<script src="js/lib/instafeed.js"></script>
+<script src="js/form.js"></script>
 <?php include('footer.php'); ?>
--- dev/footer.php
+++ dev/footer.php
@@ -13,6 +13,33 @@
         </div>
 
         <p>Subscribe to our newsletter and always be the first to know about what's happening at ROOT.</p>
+
+
+        <div id="mc_embed_signup " >
+          <form action="//rootnyc.us8.list-manage.com/subscribe/post?u=8dbb27d3d250ce738105f92ca&amp;id=118fe0b5ab" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate o-mc-form" target="_blank" novalidate >
+            <div id="mc_embed_signup_scroll" style="display: inline;">
+
+              <div class="o-mc-form__fields">
+                <input style="display: inline;" type="email" placeholder="Enter your email" value="Enter your email" name="EMAIL" class="required white email" id="mce-EMAIL">
+
+                <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="c-btn c-btn--white c-btn--mini">
+              </div>
+
+
+
+              <div id="mce-responses">
+                <div class="response" id="mce-error-response" style="display:none"></div>
+                <div class="response" id="mce-success-response" style="display:none"></div>
+              </div>
+              <div style="position: absolute; left: -5000px;"><input type="text" name="b_8dbb27d3d250ce738105f92ca_118fe0b5ab" tabindex="-1" value=""></div>
+
+            </div>
+
+          </form>
+        </div>
+
+
+        <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
       </div>
 
       <div class="c-site-footer__get-in-touch">
